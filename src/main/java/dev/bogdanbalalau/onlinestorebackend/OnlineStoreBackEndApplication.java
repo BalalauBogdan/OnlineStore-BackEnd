eada29ab68412c289a5d8eb35afbd093c5ce0a87
BalalauBogdan <bogdan262003@gmail.com>
2023-12-14 21:13:27 +0200

    OnlineStore-BackEnd App First Commit

---
+++ src/main/java/dev/bogdanbalalau/onlinestorebackend/OnlineStoreBackEndApplication.java
@@ -0,0 +1,13 @@
+package dev.bogdanbalalau.onlinestorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineStoreBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/bogdanbalalau/onlinestorebackend/OnlineStoreBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bogdanbalalau.onlinestorebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
